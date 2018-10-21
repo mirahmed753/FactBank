bc7323964dfa0e63510ea6e82411bf6f441867a4
Mir Ahmed <mirahmed753@gmail.com>
2018-10-20 21:16:54 -0400

    Completed the application's UI and functionality

--- FactBank/ViewController.h
+++ FactBank/ViewController.h
@@ -10,6 +10,9 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+- (IBAction)randomFact:(id)sender;
 
 @end
 
--- FactBank/ViewController.m
+++ FactBank/ViewController.m
@@ -20,4 +20,17 @@
 }
 
 
+- (IBAction)randomFact:(id)sender {
+    
+    NSArray *facts = @[@"The average person walks the equivalent of 5 laps around the world during their lifetime.",
+                      @"The average bolt of lightning contains enough energy to toast 100,000 pieces of bread.",
+                      @"A New Jersey man flunked out of law school and subsequently sued the school for having accepted him in the first place.",
+                      @"Simply taking 1 step uses over 200 muscles in the body.",
+                      @"Due to a genetic defect, cats can’t taste sweet things."];
+    
+    int randomNum = arc4random() % facts.count;
+    
+    self.label.text = facts[randomNum];
+    
+}
 @end
